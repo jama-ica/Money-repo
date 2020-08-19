3d8d9183bf2ed77ea098e0c34c60e09451d3ca8f
hiro <hoge>
2020-08-19 15:52:13 +0900

    models and admin page

--- money_repo/cms/admin.py
+++ money_repo/cms/admin.py
@@ -16,31 +16,97 @@ from .models import Expense
 
 # Bank
 class BankAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'note',)
-    list_display_links = ('id', 'name', 'note',)
+	list_display = ('id', 'name', 'note',)
+	list_display_links = ('id', 'name', 'note',)
 
 admin.site.register(Bank, BankAdmin)
 
+
 # BankShop
 class BankShopAdmin(admin.ModelAdmin):
-    list_display = ('id', 'bank', 'name', 'bank_shop_number', 'note',)  # 一覧に出したい項目
-    list_display_links = ('id', 'bank', 'name', 'bank_shop_number', 'note',)  # 修正リンクでクリックできる項目
-    raw_id_fields = ('bank',)   # 外部キーをプルダウンにしない（データ件数が増加時のタイムアウトを予防）
+	list_display = ('id', 'bank', 'name', 'bank_shop_number', 'note',)  # 一覧に出したい項目
+	list_display_links = ('id', 'bank', 'name', 'bank_shop_number', 'note',)  # 修正リンクでクリックできる項目
+	raw_id_fields = ('bank',)   # 外部キーをプルダウンにしない（データ件数が増加時のタイムアウトを予防）
 
 admin.site.register(BankShop, BankShopAdmin)
 
 
-admin.site.register(BankPayee)
+# BankPayee
+class BankPayeeAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'note',)
+	list_display_links = ('id', 'name', 'note',)
+
+admin.site.register(BankPayee, BankPayeeAdmin)
 
 
+# BankPaysource
+class BankPaysourceAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'note',)
+	list_display_links = ('id', 'name', 'note',)
 
-admin.site.register(BankPaysource)
+admin.site.register(BankPaysource, BankPaysourceAdmin)
 
-admin.site.register(BankBook)
-admin.site.register(BankBookIn)
-admin.site.register(BankBookOut)
-admin.site.register(IncomeKind)
-admin.site.register(Income)
-admin.site.register(ExpenseKind)
-admin.site.register(PayMethod)
-admin.site.register(Expense)
+
+# BankBook
+class BankBookAdmin(admin.ModelAdmin):
+	list_display = ('id', 'bank_shop', 'bank_book_number', 'note',)
+	list_display_links = ('id', 'bank_shop', 'bank_book_number', 'note',)
+
+admin.site.register(BankBook, BankBookAdmin)
+
+
+# BankBookIn
+class BankBookInAdmin(admin.ModelAdmin):
+	list_display = ('id', 'bank_book', 'bank_payee', 'amount', 'date', 'note',)
+	list_display_links = ('id', 'bank_book', 'bank_payee', 'amount', 'date', 'note',)
+
+admin.site.register(BankBookIn, BankBookInAdmin)
+
+
+# BankBookOut
+class BankBookOutAdmin(admin.ModelAdmin):
+	list_display = ('id', 'bank_book', 'bank_paysource', 'amount', 'date', 'note',)
+	list_display_links = ('id', 'bank_book', 'bank_paysource', 'amount', 'date', 'note',)
+
+admin.site.register(BankBookOut, BankBookOutAdmin)
+
+
+# IncomeKind
+class IncomeKindAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'note',)
+	list_display_links = ('id', 'name', 'note',)
+
+admin.site.register(IncomeKind, IncomeKindAdmin)
+
+
+# Income
+class IncomeAdmin(admin.ModelAdmin):
+	list_display = ('id', 'bank_book_in', 'income_kind', 'amount', 'date', 'note',)
+	list_display_links = ('id', 'bank_book_in', 'income_kind', 'amount', 'date', 'note',)
+
+admin.site.register(Income, IncomeAdmin)
+
+
+# ExpenseKind
+class ExpenseKindAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'note',)
+	list_display_links = ('id', 'name', 'note',)
+
+admin.site.register(ExpenseKind, ExpenseKindAdmin)
+
+
+# PayMethod
+class PayMethodAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'note',)
+	list_display_links = ('id', 'name', 'note',)
+
+admin.site.register(PayMethod, PayMethodAdmin)
+
+
+# Expense
+class ExpenseAdmin(admin.ModelAdmin):
+	list_display = ('id', 'bank_book_in', 'expense_kind', 'pay_method', 'amount', 'date', 'note',)
+	list_display_links = ('id', 'bank_book_in', 'expense_kind', 'pay_method', 'amount', 'date', 'note',)
+
+admin.site.register(Expense, ExpenseAdmin)
+
--- money_repo/cms/models.py
+++ money_repo/cms/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Bank Pay Target
 class Bank(models.Model):
 	name = models.CharField(blank=False, max_length=64)
 	note = models.CharField(blank=True, max_length=256, editable=True)
@@ -8,24 +7,42 @@ class Bank(models.Model):
 	def __str__(self):
 		return self.name
 
+
 class BankShop(models.Model):
 	bank = models.ForeignKey(Bank, on_delete=models.CASCADE)
 	name = models.CharField(blank=False, max_length=64)
 	bank_shop_number = models.PositiveSmallIntegerField(blank=False)
 	note = models.CharField(blank=True, max_length=256, editable=True)
 
+	def __str__(self):
+		return str(self.bank) + ' ' + self.name
+
+
+# Bank Pay Target
 class BankPayee(models.Model):
 	name = models.CharField(blank=False, max_length=64, default='')
 	note = models.CharField(blank=True, max_length=256, default='', editable=True)
+
+	def __str__(self):
+		return self.name
+
 
 class BankPaysource(models.Model):
 	name = models.CharField(blank=False, max_length=64)
 	note = models.CharField(blank=True, max_length=256, editable=True)
 
+	def __str__(self):
+		return self.name
+
+
 class BankBook(models.Model):
 	bank_shop = models.ForeignKey(BankShop, on_delete=models.CASCADE)
 	bank_book_number = models.PositiveIntegerField(blank=False)
 	note = models.CharField(blank=True, max_length=256, editable=True)
+
+	def __str__(self):
+		return str(self.bank_shop) + ' ' + str(self.bank_book_number)
+
 
 class BankBookIn(models.Model):
 	bank_book = models.ForeignKey(BankBook, on_delete=models.CASCADE)
@@ -34,6 +51,10 @@ class BankBookIn(models.Model):
 	date = models.DateTimeField(blank=False)
 	note = models.CharField(blank=True, max_length=256, editable=True)
 
+	def __str__(self):
+		return str(self.date) + ' ' + str(self.amount)
+
+
 class BankBookOut(models.Model):
 	bank_book = models.ForeignKey(BankBook, on_delete=models.CASCADE)
 	bank_paysource = models.ForeignKey(BankPaysource, on_delete=models.SET_NULL, blank=True, null=True)
@@ -41,9 +62,17 @@ class BankBookOut(models.Model):
 	date = models.DateTimeField(blank=False)
 	note = models.CharField(blank=True, max_length=256, editable=True)
 
+	def __str__(self):
+		return str(self.date) + ' ' + str(self.amount)
+
+
 class IncomeKind(models.Model):
 	name = models.CharField(blank=False, max_length=64)
 	note = models.CharField(blank=True, max_length=256, editable=True)
+
+	def __str__(self):
+		return self.name
+
 
 class Income(models.Model):
 	bank_book_in = models.ForeignKey(BankBookIn, on_delete=models.CASCADE)
@@ -52,13 +81,25 @@ class Income(models.Model):
 	date = models.DateTimeField(blank=False)
 	note = models.CharField(blank=True, max_length=256, editable=True)
 
+	def __str__(self):
+		return str(self.amount)
+
+
 class ExpenseKind(models.Model):
 	name = models.CharField(blank=False, max_length=64)
 	note = models.CharField(blank=True, max_length=256, editable=True)
 
+	def __str__(self):
+		return self.name
+
+
 class PayMethod(models.Model):
 	name = models.CharField(blank=False, max_length=64)
 	note = models.CharField(blank=True, max_length=256, editable=True)
+
+	def __str__(self):
+		return self.name
+
 
 class Expense(models.Model):
 	bank_book_in = models.ForeignKey(BankBookIn, on_delete=models.CASCADE)
@@ -67,3 +108,7 @@ class Expense(models.Model):
 	amount = models.PositiveIntegerField(blank=False)
 	date = models.DateTimeField(blank=False)
 	note = models.CharField(blank=True, max_length=256, editable=True)
+
+	def __str__(self):
+		return str(self.amount)
+
