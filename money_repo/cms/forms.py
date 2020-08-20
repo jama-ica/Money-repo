422e77c25666ae6628f0ec35064a16a252c9bdfb
hiro <hoge>
2020-08-20 14:35:53 +0900

    ModelChoiceField

--- money_repo/cms/forms.py
+++ money_repo/cms/forms.py
@@ -1,15 +1,20 @@
 from django import forms
+#
+from .models import Bank
+
+#class SampleChoiceForm(forms.Form):
+#	select = forms.fields.ChoiceField(
+#		choices = (
+#			('ja', '日本'),
+#			('us', 'アメリカ'),
+#			('uk', 'イギリス'),
+#			('ch', '中国'),
+#			('kr', '韓国')
+#		),
+#		required=True,
+#		widget=forms.widgets.Select
+#	)
 
 class SampleChoiceForm(forms.Form):
-	choice1 = forms.fields.ChoiceField(
-		choices = (
-			('ja', '日本'),
-			('us', 'アメリカ'),
-			('uk', 'イギリス'),
-			('ch', '中国'),
-			('kr', '韓国')
-		),
-		required=True,
-		widget=forms.widgets.Select
-	)
+	select = forms.ModelChoiceField(Bank.objects)
 
